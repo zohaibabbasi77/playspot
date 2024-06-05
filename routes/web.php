d71d08e71e91b4dc45f1002b694f506846e37fd7
ZOHAIB ABBASI <zohaibabbasi77@gmail.com>
2024-06-05 09:42:24 +0500

    uploading changes for  playspot to github

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/welcome', function () {
+    return view('welcome');
 });
